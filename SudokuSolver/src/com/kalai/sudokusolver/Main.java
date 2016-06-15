553cc003342c01c3dfc7b3eb51f9a230841a44ec
Kalai <cse.kalaivani@gmail.com>
2016-06-15 23:59:07 +0530

    initial commit

---
+++ SudokuSolver/src/com/kalai/sudokusolver/Main.java
@@ -0,0 +1,19 @@
+package com.kalai.sudokusolver;
+
+/**
+*
+* @author Kalai
+*/
+public class Main {
+
+   /**
+    * @param args the command line arguments
+    */
+   public static void main(String[] args) {
+       //Calling the Code to launch the Application Window
+       PuzzleWindow window= new PuzzleWindow();
+       System.out.println("Start");
+       window.launchApplication();
+   }
+
+}
